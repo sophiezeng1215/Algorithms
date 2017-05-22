101ff51d2756a74702a6e2bc765bcc7b22eea974
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-05-21 21:08:57 -0700

    Create TwoSum.py

---
+++ Algorithm/TwoSum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for i in range(len(nums)):
+            rem = target - nums[i]
+            if rem in nums_dict:
+                return [nums_dict[rem], i]
+            nums_dict.update({nums[i]: i})
+        return None
