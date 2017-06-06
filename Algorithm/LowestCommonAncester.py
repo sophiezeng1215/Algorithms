6d453e88674c0dabd367ddcf0693722bc4d16c1c
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-06-06 00:11:20 -0400

    Update LowestCommonAncester.py

--- Algorithm/LowestCommonAncester.py
+++ Algorithm/LowestCommonAncester.py
@@ -1,4 +1,33 @@
-# Method 1: use a stack and post-order traversal to find paths of p and q
+#Method 1: recursive call; "report" to the parent whether p or q or find. Space O(n) - worse case for unbalanced tree and O(lgn) for balanced tree, time O(n)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        elif left:
+            return left
+        else:
+            return right
+
+
+# Method 2: use a stack and post-order traversal to find paths of p and q
 
 # Definition for a binary tree node.
 # class TreeNode(object):
