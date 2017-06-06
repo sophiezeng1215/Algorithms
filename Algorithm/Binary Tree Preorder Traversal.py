1a86ae54167463c1c371e5ed30bf0ec901b9d197
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-06-06 13:18:42 -0400

    Create Binary Tree Preorder Traversal.py

---
+++ Algorithm/Binary Tree Preorder Traversal.py
@@ -0,0 +1,28 @@
+#Time O(n), space O(n)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root == []:
+            return 
+        stack = []
+        vals = []
+        node = root
+        while stack or node:
+            if node:
+                vals.append(node.val)
+                stack.append(node.right)
+                node = node.left
+            else:
+                node = stack.pop()
+        return vals
