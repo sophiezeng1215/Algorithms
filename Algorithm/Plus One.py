c6e30d7d398e987e1dcc4250272fe30d2ce3bf39
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-06-20 23:27:16 -0400

    Create Plus One.py

---
+++ Algorithm/Plus One.py
@@ -0,0 +1,18 @@
+# Time O(n)
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        if not digits:
+            return
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        res = [0]*(len(digits)+1)
+        res[0] = 1
+        return res
