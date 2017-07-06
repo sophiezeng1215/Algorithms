0c77c55dd27d82ac5a75383533f66cbf3a1816dc
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-07-06 09:59:35 -0400

    Update Longest Common Prefix.py

--- Algorithm/Longest Common Prefix.py
+++ Algorithm/Longest Common Prefix.py
@@ -1,4 +1,25 @@
 # Time O(n), space O(1)
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+        for i in range(len(strs[0])):
+            for string in strs:
+                if i >= len(string) or string[i] != strs[0][i]:
+                    if i == 0:
+                        return ''
+                    else:
+                        return strs[0][:i]
+        return strs[0]
+
+
+
+# using a sameflag
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
