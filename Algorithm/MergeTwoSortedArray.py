cb2f1b2fcd60479e4d91b44933485dddf8273f30
sophiezeng1215 <sophie.zengxiaofei@gmail.com>
2017-09-25 21:44:41 -0700

    Update MergeTwoSortedArray.py

--- Algorithm/MergeTwoSortedArray.py
+++ Algorithm/MergeTwoSortedArray.py
@@ -20,3 +20,26 @@ class Solution(object):
             pos -= 1
         if i < 0:
             nums1[:pos+1] = nums2[:j+1]
+           
+        
+# solution for removing duplicates
+
+def merge(nums1, m, nums2, n):
+    pos = m + n - 1
+    i = m - 1
+    j = n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums2[pos] = nums1[i]
+            i -= 1
+        if nums1[i] == nums2[j]:
+            nums2[pos] = nums1[i]
+            i -= 1
+            j -= 1
+        else:
+            nums2[pos] = nums2[j]
+            j -= 1
+        pos -= 1
+    if j < 0:
+        nums2[:pos+1] = nums1[:i+1]
+    print(nums2)
